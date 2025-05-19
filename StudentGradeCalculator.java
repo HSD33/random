fe3cb8f20e462dd5c46b82783b6589feb55b89ec
spindablocktrey <hdhinds33@gmail.com>
2025-05-19 19:17:34 -0400

    StudentGradeCalculator.java

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IS147ProjectCode {
+public class StudentGradeCalculator {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
  
